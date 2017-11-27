7ae577c50feeb73b369146073418fc3949044510
cszatma <christopherszatmary@gmail.com>
2017-11-27 00:04:59 -0500

    Added congruence modulo n and divides operators

---
+++ Sources/Operators.swift
@@ -0,0 +1,24 @@
+//
+//  Operators.swift
+//  DiscreteMathematics
+//
+// Created by Christopher Szatmary on 2017-11-26.
+// Copyright (c) 2017 DiscreteMathematics. All rights reserved.
+//
+
+import Foundation
+
+infix operator ==%: ComparisonPrecedence
+infix operator |%: ComparisonPrecedence
+
+/// Congruence Modulo n relation.
+/// ∀ a, b ∈ Z, a ≡ b ↔ m | (b - a).
+public func ==% (lhs: Int, rhs: (b: Int, m: Int)) -> Bool {
+    return (rhs.b - lhs) % rhs.m == 0
+}
+
+/// Divides operator. Returns true if a divides b.
+/// ∃ q ∈ N, b = q · a
+public func |% (lhs: Int, rhs: Int) -> Bool {
+    return rhs % lhs == 0
+}
